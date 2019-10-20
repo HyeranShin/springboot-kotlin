37e40b40b30f0817cc7f47043c9a6893402472de
HyeranShin <hyeran9712@naver.com>
2019-10-20 16:57:50 +0900

    Create Project Dependencies - Spring Web - Spring Data JPA - MySQL Driver

---
+++ src/test/kotlin/com/hyeran/study/StudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyeran.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
